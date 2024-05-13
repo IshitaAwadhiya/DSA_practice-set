7f2d8f93504562849b6e5136cbe09ab102e46910
IshitaAwadhiya <ishitaawadhiya@gmail.com>
2024-05-13 17:23:19 +0530

    source code

---
+++ pivotElement.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int getPivot(int arr[], int size)
+{
+    int s = 0;
+    int e = size - 1;
+    int mid = s + (e - s) / 2;
+
+    while (s < e)
+    {
+        if (arr[mid] > arr[0])
+        {
+            s = mid + 1;
+        }
+        else
+        {
+            e = mid;
+        }
+        mid = s + (e - s) / 2;
+    }
+    return s;
+}
+
+int main()
+{
+    int arr[] = {2, 3, 5, 6, 9, 1};
+    cout << "the pivot element is:" << getPivot(arr, 6);
+}
