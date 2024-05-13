2306afaebe45ac997d8c653cdc52d5a77d0e0c50
IshitaAwadhiya <ishitaawadhiya@gmail.com>
2024-05-13 13:41:29 +0530

    source code

---
+++ create_Linkedlist.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+
+class Node
+{
+	public:
+		int data;
+		Node *next;
+		
+		Node(int value){
+			data = value;
+			next = NULL;
+		}
+};
+
+int main()	
+{
+	Node *head = NULL;
+	int arr[] = {2,4,7,9,6,7};
+	for(int i=0; i<6; i++)
+		if(head == NULL){
+		head = new Node(arr[i]);
+	}else{
+		Node *temp;
+		temp = new Node(arr[i]);
+		temp->next = head;
+		head = temp;
+	}
+	
+	Node *temp = head;
+	while(temp != NULL){
+		cout<<temp->data<<" ";
+		temp = temp->next;
+	}
+	
+}
